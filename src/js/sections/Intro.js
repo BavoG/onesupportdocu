bd5a3c7941d8db1f7ec37b44b0ba9cf5223c7478
Bavo Goosens <bavo@hpe.com>
2017-02-24 13:56:13 +0100

    added anchor

--- src/js/sections/Intro.js
+++ src/js/sections/Intro.js
@@ -6,6 +6,7 @@ import Headline from 'grommet/components/Headline';
 import Box from 'grommet/components/Box';
 import Footer from 'grommet/components/Footer';
 import Image from 'grommet/components/Image';
+import Anchor from 'grommet/components/Anchor';
 
 export default function Intro () {
   return (
@@ -18,7 +19,9 @@ export default function Intro () {
       Scan the QR code for android (iOs incoming)</Headline>
       <Box direction="row" full="horizontal" justify="center" align="center"
         pad={{vertical: "medium"}} responsive={false}>
+        <Anchor href="https://mobilitycat-stg.itcs.hpe.com/catalog/uploads/com.hpeit.mobile.onesupport/Android/1.0.0/com.hpeit.mobile.onesupport.apk" target="_blank">
         <Image src="img/android.png" size="medium" colorIndex="neutral-2" />
+        </Anchor>
       </Box>
       <Footer justify="center" align="center">
         <StartButton text="scroll down to continue"/>
